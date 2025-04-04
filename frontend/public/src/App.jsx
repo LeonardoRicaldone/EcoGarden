28d4df7fb0ea3e6883954058c330ee947ac126d3
gabyceci <20190019@ricaldone.edu.sv>
2025-04-04 15:13:40 -0600

    Inicio de Nav

--- frontend/public/src/App.jsx
+++ frontend/public/src/App.jsx
@@ -1,11 +1,39 @@
-import { Routes, Route } from 'react-router-dom';2023
-import './App.css'
+import {BrowserRouter as Router, Routes, Route} 
+from 'react-router'
+
+import Home from './screens/Home'
+import Register from './screens/Register'
+import Login from './screens/Login'
+import About from './screens/About'
+import Contact from './screens/Contact'
+import Favorites from './screens/Favorites'
+import Products from './screens/Products'
+import Profile from './screens/Profile'
+import ShoppingCart from './screens/ShoppingCart'
+import TermsConditions from './screens/TermsConditions'
+import Nav from './components/Nav'
+
 
 function App() {
 
   return (
     <>
-      
+    <Router>
+|    <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Register' element={<Register />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Favorites' element={<Favorites />} />
+        <Route path='/Products' element={<Products />} />
+        <Route path='/Profile' element={<Profile />} />
+        <Route path='/ShoppingCart' element={<ShoppingCart />} />
+        <Route path='/TermsConditions' element={<TermsConditions />} />
+      </Routes>
+    </Router>
+    
     </>
   )
 }
