43f9a9d372595c0991cb432273521a326f676651
Leonardo <20230070@ricaldone.edu.sv>
2025-04-04 15:21:49 -0600

    footer

--- frontend/public/src/App.jsx
+++ frontend/public/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';2023
 import './App.css'
+import Footer from './components/Footer/Footer';
 
 function App() {
 
   return (
     <>
-      
+      <Footer/>
     </>
   )
 }
